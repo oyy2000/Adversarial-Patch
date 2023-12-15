946d8caee051edecb8a2e56d3ac798f7086b6af9
Xinhao Kong <xinhaokong@Xinhaos-MBP.lan>
2023-12-15 00:04:48 -0500

    new results with nll non_targeted

--- run.py
+++ run.py
@@ -42,6 +42,9 @@ transform = transforms.Compose([
                          std=NORM_STD)
 ])
 
+trainset = torchvision.datasets.CIFAR10(root='./data', train=True, download=True, transform=transform)
+testset = torchvision.datasets.CIFAR10(root='./data', train=False, download=True, transform=transform)
+
 
 # Function to calculate accuracy
 def calculate_accuracy(net, dataloader, device):
@@ -76,10 +79,8 @@ def vanilla_model_test(net):
 
 def place_patch(img, patch):
     for i in range(img.shape[0]):
-        # h_offset = np.random.randint(0,img.shape[2]-patch.shape[1]-1)
-        # w_offset = np.random.randint(0,img.shape[3]-patch.shape[2]-1)
-        h_offset = int(0.33 * (img.shape[2] - patch.shape[1]))
-        w_offset = int(0.33 * (img.shape[3] - patch.shape[2]))
+        h_offset = np.random.randint(0,img.shape[2]-patch.shape[1]-1)
+        w_offset = np.random.randint(0,img.shape[3]-patch.shape[2]-1)
         TENSOR_MEANS, TENSOR_STD = torch.FloatTensor(NORM_MEAN)[:,None,None], torch.FloatTensor(NORM_STD)[:,None,None]
         img[i,:,h_offset:h_offset+patch.shape[1],w_offset:w_offset+patch.shape[2]] = (torch.tanh(patch) + 1 - 2 * TENSOR_MEANS) / (2 * TENSOR_STD)
     return img
@@ -97,18 +98,25 @@ def evaluate(net, patch, testloader):
             total += labels.shape[0]
     return ("%.3f" % (correct.item() / total))
 
-def eval_patch(net, patch, testloader, target_class):
-    net.eval()
-    tp, tp_5, total = 0, 0, 0
+def eval_patch(model, patch, val_loader, target_class):
+    model.eval()
+    tp, tp_5, counter = 0., 0., 0.
     with torch.no_grad():
-        for img, labels in tqdm(testloader, leave=False):
-            img = place_patch(img, patch)
-            img, labels = img.to(device), labels.to(device)
-            pred = net(img)
-            tp += torch.logical_and(pred.argmax(dim=-1) == target_class, labels != target_class).sum()
-            tp_5 += torch.logical_and((pred.topk(5, dim=-1)[1] == target_class).any(dim=-1), labels != target_class).sum()
-            total += (labels != target_class).sum()
-    return ("%.3f" % (tp.item() / total.item())), ("%.3f" % (tp_5.item() / total.item()))
+        for img, img_labels in tqdm(val_loader, desc="Validating...", leave=False):
+            # For stability, place the patch at 4 random locations per image, and average the performance
+            for _ in range(4): 
+                patch_img = place_patch(img, patch)
+                patch_img = patch_img.to(device)
+                img_labels = img_labels.to(device)
+                pred = model(patch_img)
+                # In the accuracy calculation, we need to exclude the images that are of our target class
+                # as we would not "fool" the model into predicting those
+                tp += torch.logical_and(pred.argmax(dim=-1) == target_class, img_labels != target_class).sum()
+                tp_5 += torch.logical_and((pred.topk(5, dim=-1)[1] == target_class).any(dim=-1), img_labels != target_class).sum()
+                counter += (img_labels != target_class).sum()
+    acc = tp/counter
+    top5 = tp_5/counter
+    return ("%.3f" % (acc.item())), ("%.3f" % (top5.item()))
 
 def eval_patch_untargeted(net, patch, testloader):
     net.eval()
@@ -144,8 +152,60 @@ def train_patch_untargeted(net, trainloader, patch_size, num_epochs):
         print (f'Epoch {epoch+1}/{num_epochs}, Loss: {loss.item():.4f}')
     return patch
 
+def patch_attack_untargeted(model, patch_size, num_epochs):
+    train_loader = torch.utils.data.DataLoader(trainset, batch_size=BATCH_SIZE, shuffle=True, drop_last=True, num_workers=NUM_WORKERS)
 
-def train_patch(net, trainloader, target_class, patch_size, num_epochs):
+    if not isinstance(patch_size, tuple):
+        patch_size = (patch_size, patch_size)
+    
+    patch = nn.Parameter(torch.zeros(3, patch_size[0], patch_size[1]), requires_grad=True)
+    optimizer = torch.optim.Adam([patch], lr=0.01)
+    for epoch in range(num_epochs):
+        t = tqdm(train_loader, leave=False)
+        for img, labels in t:
+            img = place_patch(img, patch)
+            img = img.to(device)
+            labels = labels.to(device)
+            pred = model(img)
+            loss = -torch.nn.functional.nll_loss(pred, labels)
+            optimizer.zero_grad()
+            loss.mean().backward()
+            optimizer.step()
+            t.set_description(f"Epoch {epoch}, Loss: {loss.item():4.2f}")
+        print(f"Epoch [{epoch+1}/{num_epochs}], Average Loss: {loss.mean():.4f}")
+    return patch
+
+
+def patch_attack(model, target_class, patch_size, num_epochs):
+    train_loader = torch.utils.data.DataLoader(trainset, batch_size=BATCH_SIZE, shuffle=True, drop_last=True, num_workers=NUM_WORKERS)
+    val_loader = torch.utils.data.DataLoader(testset, batch_size=BATCH_SIZE, shuffle=False, drop_last=False, num_workers=NUM_WORKERS)
+
+    # Create parameter and optimizer
+    if not isinstance(patch_size, tuple):
+        patch_size = (patch_size, patch_size)
+    patch = nn.Parameter(torch.zeros(3, patch_size[0], patch_size[1]), requires_grad=True)
+    # optimizer = torch.optim.SGD([patch], lr=1e-1, momentum=0.8)
+    optimizer = torch.optim.Adam([patch], lr=0.01)
+    loss_module = nn.CrossEntropyLoss()
+    
+    # Training loop
+    for epoch in range(num_epochs):
+        t = tqdm(train_loader, leave=False)
+        for img, _ in t:
+            img = place_patch(img, patch)
+            img = img.to(device)
+            pred = model(img)
+            labels = torch.zeros(img.shape[0], device=pred.device, dtype=torch.long).fill_(target_class)
+            loss = loss_module(pred, labels)
+            optimizer.zero_grad()
+            loss.mean().backward()
+            optimizer.step()
+            t.set_description(f"Epoch {epoch}, Loss: {loss.item():4.2f}")
+        print(f"Epoch [{epoch+1}/{num_epochs}], Average Loss: {loss.mean():.4f}")
+    return patch
+
+def train_patch(net, target_class, patch_size, num_epochs):
+    trainloader = torch.utils.data.DataLoader(trainset, batch_size=128, shuffle=True, drop_last=True, num_workers=8)
     if not isinstance(patch_size, tuple):
         patch_size = (patch_size, patch_size)
     patch = nn.Parameter(torch.zeros(3, patch_size[0], patch_size[1]), requires_grad=True)
@@ -167,24 +227,16 @@ def train_patch(net, trainloader, target_class, patch_size, num_epochs):
     return patch
 
 def main(args):
-    if args.non_targeted:
-        UntargetedAttack(args)
-    else:
-        TargetedAttack(args)
-
-def UntargetedAttack(args):
-    CHECKPOINT_PATH = './saved_models/non_targeted/'
     model_name = args.model
-    patch_size_list = [3, 5, 7, 9, 16]
+    global CHECKPOINT_PATH
+    if args.non_targeted:
+        CHECKPOINT_PATH = './saved_models/non_targeted/'
     if model_name == "densenet121":
         print (f"Model {model_name}...")
-        # net = DenseNet.from_pretrained("densenet121")
         net = densenet121(pretrained=True)
-        # net.model.load_state_dict(torch.load(f"densenet121.pt"))
     elif model_name == "densenet161":
         print (f"Model {model_name}...")
         net = densenet161(pretrained=True)
-        # net.model.load_state_dict(torch.load(f"densenet161.pt"))
     else:
         print(f"Model: {model_name}. Target")
         net = torch.hub.load("chenyaofo/pytorch-cifar-models", f"cifar10_{model_name}", pretrained=True)
@@ -194,10 +246,16 @@ def UntargetedAttack(args):
         os.makedirs(f"{CHECKPOINT_PATH}{model_name}")
     net.to(device)
     net.eval()
+    if args.non_targeted:
+        UntargetedAttack(args, net, trainset, testset)
+    else:
+        TargetedAttack(args, net, trainset, testset)
+
+def UntargetedAttack(args, net, trainset, testset):
+    model_name = args.model
+    patch_size_list = [3, 5, 7, 9, 16]
     # Train the patch
     results_dict = {}
-    trainset = torchvision.datasets.CIFAR10(root='./data', train=True, download=True, transform=transform)
-    testset = torchvision.datasets.CIFAR10(root='./data', train=False, download=True, transform=transform)
     for patch_size in patch_size_list:
         save_patch_name = f"{CHECKPOINT_PATH}{model_name}/{patch_size}.pt"
         save_str = "patch_" + str(patch_size)
@@ -210,9 +268,10 @@ def UntargetedAttack(args):
             print (results)
             results_dict[save_str] = results
             continue
-        patch = train_patch_untargeted(net, trainloader, patch_size=patch_size, num_epochs=args.num_epochs)
+        # patch = train_patch_untargeted(net, trainloader, patch_size=patch_size, num_epochs=args.num_epochs)
         # Evaluate the patch
         # results = evaluate(net, patch, testloader)
+        patch = patch_attack_untargeted(net, patch_size=patch_size, num_epochs=args.num_epochs)
         results = eval_patch_untargeted(net, patch, testloader)
         print (results)
         results_dict[save_str] = results
@@ -223,39 +282,18 @@ def UntargetedAttack(args):
         json.dump(results_dict, f)
     print(f"Results saved to {CHECKPOINT_PATH}{model_name}_results.json")
 
-def TargetedAttack(args):
+def TargetedAttack(args, net, trainset, testset):
     model_name = args.model
+    # patch_size_list = [3]
     patch_size_list = [3, 5, 7, 9, 16]
-    if model_name == "densenet121":
-        print (f"Model {model_name}...")
-        # net = DenseNet.from_pretrained("densenet121")
-        net = densenet121(pretrained=True)
-        # net.model.load_state_dict(torch.load(f"densenet121.pt"))
-    elif model_name == "densenet161":
-        print (f"Model {model_name}...")
-        net = densenet161(pretrained=True)
-        # net.model.load_state_dict(torch.load(f"densenet161.pt"))
-    else:
-        print(f"Model: {model_name}. Target")
-        net = torch.hub.load("chenyaofo/pytorch-cifar-models", f"cifar10_{model_name}", pretrained=True)
-    # check if the directory exists
-    if not os.path.exists(CHECKPOINT_PATH):
-        os.makedirs(CHECKPOINT_PATH)
-    if not os.path.exists(f"{CHECKPOINT_PATH}{model_name}"):
-        os.makedirs(f"{CHECKPOINT_PATH}{model_name}")
-    net.to(device)
-    net.eval()
     # Train the patch
     results_dict = {}
-    trainset = torchvision.datasets.CIFAR10(root='./data', train=True, download=True, transform=transform)
-    testset = torchvision.datasets.CIFAR10(root='./data', train=False, download=True, transform=transform)
     for patch_size in patch_size_list:
         for target_class in range(NUM_CLASSES):
         # Load CIFAR-10 training and test datasets
             save_patch_name = f"{CHECKPOINT_PATH}{model_name}/{patch_size}_{target_class}.pt"
             save_str = "patch_" + str(patch_size) + "_target_" + str(target_class)
-            trainloader = torch.utils.data.DataLoader(trainset, batch_size=128, shuffle=True, drop_last=True, num_workers=NUM_WORKERS)
-            testloader = torch.utils.data.DataLoader(testset, batch_size=128, shuffle=False, drop_last=False, num_workers=NUM_WORKERS)
+            testloader = torch.utils.data.DataLoader(testset, batch_size=BATCH_SIZE, shuffle=False, drop_last=False, num_workers=NUM_WORKERS)
             if os.path.exists(save_patch_name):
                 print(f"Patch {save_str} already exists. Skipping...")
                 # results = evaluate(net, torch.load(save_patch_name), testloader)
@@ -264,7 +302,8 @@ def TargetedAttack(args):
                 print (results)
                 continue
             print(f"Target Class: {LABEL_NAMES[target_class]}")
-            patch = train_patch(net, trainloader, target_class, patch_size=patch_size, num_epochs=args.num_epochs)
+            # patch = train_patch(net, target_class, patch_size=patch_size, num_epochs=args.num_epochs)
+            patch = patch_attack(net, target_class, patch_size=patch_size, num_epochs=args.num_epochs)
             # Evaluate the patch
             # results = evaluate(net, patch, testloader)
             results = eval_patch(net, patch, testloader, target_class)
@@ -272,11 +311,6 @@ def TargetedAttack(args):
             results_dict[save_str] = results
             # Save this patch 
             torch.save(patch.data, f"{CHECKPOINT_PATH}{model_name}/{patch_size}_{target_class}.pt")
-    # Save the results_dict to a file
-    # print (results_dict)
-    # results_json = {model_name: {
-    #     patch_size: [{item : results_dict[item]["results"]} for item in range(NUM_CLASSES)],
-    # }}
     with open(f"{CHECKPOINT_PATH}{model_name}_results.json", "w") as f:
         json.dump(results_dict, f)
     print(f"Results saved to {CHECKPOINT_PATH}{model_name}_results.json")
